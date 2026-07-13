2a013eb500da31d28860d933ac0bea09b8fd9d99
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2026-07-13 10:46:31 -0300

    feat: add function to get all language options

--- transcriber.py
+++ transcriber.py
@@ -16,3 +16,24 @@ def transcribe_audio(audio_file: str) -> None:
     result = model.transcribe(audio_file, verbose=True)
     # TODO: Save the result to a file? 
     print(result["text"])
+def get_language_options() -> dict[str, str]:
+    """Get the available language options.
+
+    Returns:
+        dict[str, str]: A dictionary of available language options sorted in alphabetical order in the format {language: code}.
+    """
+    sorted_languages = dict(
+        sorted(
+            whisper.tokenizer.LANGUAGES.items(),
+            key=lambda item: item[1])
+    )
+
+    #reverses the dictionary to have the language as the key and the code as the value
+    reverse_sorted_languages = {
+        str.capitalize(language): code for code, language in sorted_languages.items()
+    }
+
+    return {
+        "Auto-detect": "auto",
+        **reverse_sorted_languages
+    }
