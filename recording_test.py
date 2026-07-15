67af99dc43a5cd9fdf56cf18c974a913aa112c8e
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2026-07-15 15:45:19 -0300

    doc: add useful comments

--- recording_test.py
+++ recording_test.py
@@ -1,16 +1,20 @@
 import sounddevice as sd
 import whisper
 
+# sounddevice doc: https://python-sounddevice.readthedocs.io/en/0.5.3/usage.html
+# useful: https://python-sounddevice.readthedocs.io/en/0.5.3/examples.html#recording-with-arbitrary-duration
+
 print(
     f"Devices: {sd.query_devices()}\nDefault input device: {sd.default.device[0]}"
 )
 
 
-# Whisper expectes the numpy array audio to have a 16k samplerate, so for we just record it like that
+# Whisper expectes the numpy array audio to have a 16k samplerate, so for now we just record it like that. We could instead resample it to 16k but idk, yk.
 fs = 16000  # Sample rate
 sd.default.samplerate = fs  # Sample rate
 seconds = 30  # Duration of recording
 dtype = "float32"  # Data type of the recording
+channels = 1  # Number of channels (1 for mono, 2 for stereo)
 
 
 print("Recording...")
@@ -18,7 +22,7 @@ print("Recording...")
 # sd.RawInputStream()
 myrecording = sd.rec(
     int(seconds * fs),
-    channels=1,
+    channels=channels,
     dtype=dtype,
     blocking=True
 )
